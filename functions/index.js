bd334828981cf97d7b5a40519a5ba4ca661a6f73
Nicolas <nikkolasnv1997@gmail.com>
2020-04-18 00:23:22 -0400

    like/unlike/delete scream

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,9 @@ const {
   postOneScream,
   getScream,
   commentOnScream,
+  likeScream,
+  unlikeScream,
+  deleteScream,
 } = require("./handlers/screams");
 
 const {
@@ -23,9 +26,9 @@ const {
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
 app.get("/scream/:screamId", getScream);
-// TODO: delete scream
-// TODO: like scream
-// TODO: unlike scream
+app.delete("/scream/:screamId", FBAuth, deleteScream);
+app.get("/scream/:screamId/like", FBAuth, likeScream);
+app.get("/scream/:screamId/unlike", FBAuth, unlikeScream);
 // TODO: comment on scream
 app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
 
